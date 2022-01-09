b4350c740f356bc8566d716b41748f148349e2c7
JunHyeokYu <45321171+tomejake@users.noreply.github.com>
2022-01-09 23:22:04 +0900

    테스트코드 작성

--- src/test/java/com/web/JpaMappingTest.java
+++ src/test/java/com/web/JpaMappingTest.java
@@ -1,11 +1,19 @@
 package com.web;
 
+import com.web.domain.Board;
 import com.web.domain.User;
+import com.web.domain.enums.BoardType;
 import com.web.repository.BoardRepository;
 import com.web.repository.UserRepository;
 import org.aspectj.lang.annotation.Before;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.*;
 
 
 @DataJpaTest
@@ -19,5 +27,25 @@ public class JpaMappingTest {
     @Autowired
     BoardRepository boardRepository;
 
+    @Before("")
+    public void init(){
+        User user = userRepository.save(User.builder().name("havi").password("test").email(email).createdDate(LocalDateTime.now()).build());
+
+        Board board = boardRepository.save(Board.builder().title(boardTestTitle).subTitle("서브 타이틀").content("콘텐츠").boardType(BoardType.free).createdDate(LocalDateTime.now()).updatedDate(LocalDateTime.now()).user(user).build());
+    }
+
+    @Test
+    public void 생성_테스트(){
+        User user = userRepository.findByEmail(email);
+        assertThat(user.getName()).isEqualTo("havi");
+        assertThat(user.getPassword()).isEqualTo("test");
+        assertThat(user.getEmail()).isEqualTo(email);
+
+        Board board = boardRepository.findByUser(user);
+        assertThat(board.getTitle()).isEqualTo(boardTestTitle);
+        assertThat(board.getSubTitle()).isEqualTo("서브 타이틀");
+        assertThat(board.getContent()).isEqualTo("콘텐츠");
+        assertThat(board.getBoardType()).isEqualTo(BoardType.free);
+    }
 
 }
