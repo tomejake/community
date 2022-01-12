0743dd4dbf42bc2d6eda7bc7ab1b266c65ad3960
tomejake <tomejake22@gmail.com>
2022-01-12 09:58:57 +0900

    BoardController 구현

---
+++ src/main/java/com/web/controller/BoardController.java
@@ -0,0 +1,31 @@
+package com.web.controller;
+
+import com.web.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/board")
+public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping({"", "/"})
+    public String board(@RequestParam(value = "idx", defaultValue = "0") Long idx, Model model){
+        model.addAttribute("board", boardService.findBoardByIdx(idx));
+        return "/board/form";
+    }
+
+    @GetMapping("list")
+    public String list(@PageableDefault Pageable pageable, Model model) {
+        model.addAttribute("boardList", boardService.findBoardList(pageable));
+        return "/board/list";
+    }
+}
