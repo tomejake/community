1800b487d00b3974b96239465ac16df0a3406279
tomejake <tomejake22@gmail.com>
2022-01-14 10:56:48 +0900

    수정

--- front/src/components/BoardComponent.vue
+++ front/src/components/BoardComponent.vue
@@ -1,29 +1,29 @@
 <template>
-  <div class="container"></div>
-  <h1 class="text-center">Board List</h1>
-  <table>
-      <thead>
-        <th>#</th>
-        <th>서비스 분류</th>
-        <th>제목</th>
-        <th>작성 날짜</th>
-      </thead>
-      <tbody>
-          <tr v-for="item in board" :key="item.idx">
-            <td>{{board.idx}}</td>
-            <td>{{board.boardType}}</td>
-            <td>{{board.title}}</td>
-            <td>{{board.createDate}}</td>
-          </tr>
-      </tbody>
-  </table>
+<div class="container"></div>
+<h1 class="text-center">Board List</h1>
+<table>
+    <thead>
+    <th>#</th>
+    <th>서비스 분류</th>
+    <th>제목</th>
+    <th>작성 날짜</th>
+    </thead>
+    <tbody>
+        <tr v-for="item in board" :key="item.idx">
+        <td>{{item.idx}}</td>
+        <td>{{item.boardType}}</td>
+        <td>{{item.title}}</td>
+        <td>{{item.createDate}}</td>
+        </tr>
+    </tbody>
+</table>
 </template>
 
 <script>
 import BoardService from '../services/BoardService.js'
 
 export default {
-    name: 'Board',
+    name: 'BoardComponent',
     data(){
         return {
             board : []
@@ -32,7 +32,7 @@ export default {
     methods: {
         getBoard(){
         BoardService.getBoard().then((response) => {
-                this.board = response.data;   
+                this.board = response.data.content;
             });
         }
     },
